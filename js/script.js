aee5ed2af4bbed1410ba7a5e08de36260e6436da
Abhilasha Agrawal <abhilashaagrawal27@gmail.com>
2023-01-26 14:22:20 +0530

    news

---
+++ js/script.js
@@ -0,0 +1,24 @@
+//js object
+
+  let news1 ={
+    w1: "The",
+    w2: 'IL-3SD',
+    w3: `aircraft`,
+    w4: 'was',
+    w5: `decommissioned`,
+    w6: "on",
+    w7: 'january',
+    w8: `17,2022,`,
+    w9:'after',
+    w10:"completing",
+    w11: `44`,
+    w12: "glorious",
+    w13: 'years',
+    w14: 'of',
+    w15: `service`,
+    w16: 'to',
+    w17: 'the',
+    w18: "nation"
+}
+var s=news1.w1+" "+news1.w2+" "+news1.w3+" "+news1.w4+" "+news1.w5+" "+news1.w6+" "+news1.w7+" "+news1.w8+" "+news1.w9+" "+news1.w10+" "+news1.w11+" "+news1.w12+" "+news1.w13+" "+news1.w14+" "+news1.w15+" "+news1.w16+" "+news1.w17+" "+news1.w18;
+console.log(s);
